2596eff24749133713c201b69465ba18ed00e487
Rene Donner <code@donner.at>
2014-11-04 22:30:38 +0100

    fix relative path

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-with import /home/rene/nixpkgs/default.nix {}; rec {
+with import ../nixpkgs/default.nix {}; rec {
   lmdb = callPackage ./lmdb {};  
   cudatoolkit6_5 = callPackage ./cudatoolkit/6.5.nix {};
   caffe = callPackage ./caffe { lmdb = lmdb; cudatoolkit6_5 = cudatoolkit6_5; };
