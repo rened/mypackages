0b42944a45f62b8761adf95e7b349ffbc5c58e1e
Rene Donner <code@donner.at>
2014-11-12 15:46:42 +0100

    add julia 0.3.2 with sys.so for core2

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ with import ../nixpkgs/default.nix {}; rec {
   lmdb = callPackage ./lmdb {};  
   cudatoolkit6_5 = callPackage ./cudatoolkit/6.5.nix {};
   caffe = callPackage ./caffe { lmdb = lmdb; cudatoolkit6_5 = cudatoolkit6_5; };
-  caffe-cpu = callPackage ./caffe { lmdb = lmdb; cudatoolkit6_5 = cudatoolkit6_5; cpuonly = true; };
+  caffecpu = callPackage ./caffe { lmdb = lmdb; cudatoolkit6_5 = cudatoolkit6_5; cpuonly = true; };
   halide = callPackage ./halide {};
+  julia = callPackage ./julia/0.3.2.nix {};
 }
